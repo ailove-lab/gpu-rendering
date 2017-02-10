9ebbdcbd17ef6b8f2fb154301771fdeb9e4fd00f
peko <gasu.basu@gmail.com>
2017-02-10 18:52:23 +0300

    update

--- egl/egl.c
+++ egl/egl.c
@@ -11,21 +11,14 @@
 #include "colors.h"
 #include "egl.h"
 
-//static const EGLint configAttribs[] = {
-//    EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
-//    EGL_BLUE_SIZE , 8,
-//    EGL_GREEN_SIZE, 8,
-//    EGL_RED_SIZE  , 8,
-//    EGL_ALPHA_SIZE, 8,
-//    EGL_DEPTH_SIZE, 8,
-//    EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
-//    EGL_NONE
-//};
-
 static const EGLint configAttribs[] = {
+    EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
     EGL_BLUE_SIZE , 8,
     EGL_GREEN_SIZE, 8,
     EGL_RED_SIZE  , 8,
+    EGL_ALPHA_SIZE, 8,
+    EGL_DEPTH_SIZE, 8,
+    EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
     EGL_NONE
 };
 
